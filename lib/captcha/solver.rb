ca134eec4e69baa42de33aa26d2f9a09354602f2
quyetdc <quyetdc.uet@gmail.com>
2018-05-04 03:57:59 +0000

    Improve decoder engine & Add log if fail

--- lib/captcha/solver.rb
+++ lib/captcha/solver.rb
@@ -16,24 +16,36 @@ module Captcha
     def solve
       # read image from disk
       img = Magick::Image.read(@image_path).first
+      
       # process image to reduce noise and improve text quality
       img = process_image(img)
-
-      # TODO enhance img
-      # enhance the image to improve ocr accuracy
-      img = enhance(img)
 
       # save the image in disk
       img.write(@solved_path)
 
       # extract result
-      extract_characters
+      extracted_text = extract_characters
+      return extracted_text if extracted_text.match(/\A[a-zA-Z0-9]{6}\z/)
+
+    
+      img = enhance(img)
+      img.write(@solved_path)
+      extracted_text = extract_characters
+      
+      printf "CANNOT BYPASS CAPTCHA --- " unless extracted_text.match(/\A[a-zA-Z0-9]{6}\z/) 
+      extracted_text
     end
 
     def process_image(img)
       # reduce size
       # img.scale! 0.75
-
+      
+      # crop image, remove bottom part
+      img_width = img.columns
+      img_height = img.rows
+      # args X, Y, width, height
+      img = img.crop 0, 5, img_width - 5, img_height - 20
+      
       # transform image into gray scale colors
       img = img.quantize(128, Magick::GRAYColorspace)
 
@@ -44,30 +56,44 @@ module Captcha
       img = img.quantize(2, Magick::GRAYColorspace)
 
       # Add border to avoid noise there
-      img.border!(5, 5, 'white')
+      # img.border!(5, 5, 'white')
     end
 
     def enhance(img)
       # clean extra noise
-      clean_image img, 2
-
+      clean_image img, 4
       # fill blank spots
-      # fill img
+      fill(img, 2)
 
-      # soft edges
-      # img = img.gaussian_blur 0.5, 0.5
-
-      # cut white space to improve ocr accuracy
-      # trim(img)
+      # remove_stripes img
+    end
+  
+    def remove_stripes(img, range = 20)
+      color = 'black'
+      img.each_pixel do |pixel, c, r|
+        next if border?(c, range, img.columns) || border?(r, range, img.rows)
+        
+        to_change = true
+        
+        range.times do |i|
+          to_change = false if (!pixel.to_color.eql? color) || (!img.pixel_color(c+i, r+i).to_color.eql? color)
+        end
+        
+        if to_change
+           range.times do |i|
+             img.pixel_color(c+i, r+i, 'white')
+           end
+        end
+      end
     end
 
     # Fill white spots with black pixels
-    def fill(img, range = 3, color = 'black')
+    def fill(img, range, color = 'black')
       process img, color, range
     end
 
     # Clean noise
-    def clean_image(img, range = 4, color = 'white')
+    def clean_image(img, range, color = 'white')
       process img, color, range
     end
 
@@ -97,13 +123,15 @@ module Captcha
       pixel < range || pixel > (max - range)
     end
 
-    #
     def extract_characters
       text = RTesseract.new(@solved_path,
                             lang: @lang,
                             options: @options,
                             psm: @psm)
-      text.to_s_without_spaces
+      extracted_text = text.to_s_without_spaces
+      extracted_text.gsub!(/[^0-9A-Za-z]/, '')
+      extracted_text
+      
     end
   end
 end
